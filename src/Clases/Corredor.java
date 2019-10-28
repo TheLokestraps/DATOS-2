cc31d8990f0c4a9fa27333b811930e477709fbf7
TheLokestraps <ivanestebanperezdlo@hotmail.com>
2019-10-28 14:22:27 -0500

    Quizas el commit mas importante, terminado JFRAMES Y COMENZAR CODIFICACION

---
+++ src/Clases/Corredor.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author ieperez
+ */
+public class Corredor {
+    public String Nombre;
+    public String Nacionalidad;
+    public String Equipo;
+    public int Numero;
+    public int Edad;
+    public String Foto;
+    
+    public Corredor(String Nombre,String Nacion, String Equipo, int Numero, int Edad, String Foto){
+        this.Nombre = Nombre;
+        this.Equipo = Equipo;
+        this.Nacionalidad = Nacion;
+        this.Numero = Numero;
+        this.Edad = Edad;
+        this.Foto = Foto;
+    }
+    
+}
