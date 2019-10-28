94abf86c16f467d4cc43e688331a9dada45cf4ab
TheLokestraps <ivanestebanperezdlo@hotmail.com>
2019-10-28 00:29:33 -0500

    Incial

---
+++ src/lab2estructuradedatos/Lab2Estructuradedatos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2estructuradedatos;
+
+/**
+ *
+ * @author TheLokestraps
+ */
+public class Lab2Estructuradedatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
